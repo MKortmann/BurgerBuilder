aa3c72d6e26023730732691794d7c970f6402795
MKortmann <companymvms@gmail.com>
2019-09-11 20:33:58 +0200

    add: backdrop component

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -1,10 +1,8 @@
 import React from "react";
+import classes from "./backdrop.module.css"
 
-
-const modal = (props) => (
-  <div>
-    {props.children}
-  </div>
+const backdrop = (props) => (
+  props.show ? <div className={classes.Backdrop} onClick={props.clicked}></div> : null
 );
 
-export default modal;
+export default backdrop;
